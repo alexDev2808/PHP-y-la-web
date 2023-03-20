c28d0029396b8b754e143c511923b5ace8305714
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-20 00:11:29 -0600

    Almacenando contactos en la base de datos

--- add.php
+++ add.php
@@ -1,22 +1,14 @@
 
 <?php
 
+  require "database.php";
+
   if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $contact = [
-      "name" => $_POST["name"],
-      "phone" => $_POST["phone"],
+    $name = $_POST["name"];
+    $phone = $_POST["phone"];
 
-    ];
-
-    if(file_exists("contacts.json")) {
-      $contacts = json_decode(file_get_contents("contacts.json"), true);
-    }else {
-      $contacts = [];
-    }
-
-    $contacts[] = $contact;
-
-    file_put_contents( "contacts.json", json_encode($contacts) );
+    $statement = $conn->prepare("INSERT INTO contacts(name, phone_number) VALUES ('$name', '$phone')");
+    $statement->execute();
 
     header("Location: index.php");
   }
