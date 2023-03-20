48992d993c5ebee1f13e6e61954db4e92bb28406
AlexDev2808 <colores.2808.8000@gmail.com>
2023-03-20 16:19:40 -0600

    Reutilizacion de HTML con PHP

--- add.php
+++ add.php
@@ -26,87 +26,36 @@
 ?>
 
 
+<?php require "partials/header.php" ?>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" 
-            href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.3/darkly/bootstrap.min.css" 
-            integrity="sha512-YRcmztDXzJQCCBk2YUiEAY+r74gu/c9UULMPTeLsAp/Tw5eXiGkYMPC4tc4Kp1jx/V9xjEOCVpBe4r6Lx6n5dA==" 
-            crossorigin="anonymous" 
-            referrerpolicy="no-referrer" />
-    <script 
-            defer
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-            crossorigin="anonymous">
-    </script>
-
-    <link rel="stylesheet" href="./static/css/index.css">
-    <title>Contacts App</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><ion-icon name="library-outline"></ion-icon> Contacts App</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./add.php">Add Contact <ion-icon name="add-outline"></ion-icon></a>
-              </li>
-            </ul>   
-          </div>
-        </div>
-      </nav>
-
-      <main>
-        <div class="container pt-5">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="card">
-                <div class="card-header">Add New Contact</div>
-                <div class="card-body">
-                  <?php if ($error) : ?>
-                    <p class="text-danger">
-                      <?= $error ?>
-                    </p>
-                  <?php endif ?> 
-                  <form method="POST" action="add.php">
-                    <div class="mb-3">
-                      <label for="name" class="form-label">Name</label>
-                      <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="name">
-                      <div id="nameHelp" class="form-text">Escribe tu nombre completo</div>
-                    </div>
-                    <div class="mb-3">
-                      <label for="phone" class="form-label">Phone Number</label>
-                      <input type="tel" class="form-control" id="phone" name="phone">
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </form>
-                </div>
+  <div class="container pt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Add New Contact</div>
+          <div class="card-body">
+            <?php if ($error) : ?>
+              <p class="text-danger">
+                <?= $error ?>
+              </p>
+            <?php endif ?> 
+            <form method="POST" action="add.php">
+              <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" name="name">
+                <div id="nameHelp" class="form-text">Escribe tu nombre completo</div>
               </div>
-            </div>
+              <div class="mb-3">
+                <label for="phone" class="form-label">Phone Number</label>
+                <input type="tel" class="form-control" id="phone" name="phone">
+              </div>
+              
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
         
-      </main>
-
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-</body>
-</html>
+<?php require "partials/footer.php" ?>
--- index.php
+++ index.php
@@ -7,82 +7,32 @@ $contacts = $conn->query("SELECT * FROM contacts");
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require "partials/header.php" ?> 
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" 
-            href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.3/darkly/bootstrap.min.css" 
-            integrity="sha512-YRcmztDXzJQCCBk2YUiEAY+r74gu/c9UULMPTeLsAp/Tw5eXiGkYMPC4tc4Kp1jx/V9xjEOCVpBe4r6Lx6n5dA==" 
-            crossorigin="anonymous" 
-            referrerpolicy="no-referrer" />
-    <script 
-            defer
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-            crossorigin="anonymous">
-    </script>
 
-    <link rel="stylesheet" href="./static/css/index.css">
-    <title>Contacts App</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#"><ion-icon name="library-outline"></ion-icon> Contacts App</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./add.php">Add Contact <ion-icon name="add-outline"></ion-icon></a>
-              </li>
-            </ul>   
-          </div>
-        </div>
-      </nav>
-
-      <main>
-        <div class="container pt-4 p-3">
-            <div class="row">
-              <?php if($contacts->rowCount() == 0): ?>
-                <div class="col-md-4 mx-auto">
-                  <div class="card card-body text-center">
-                    <p>Sin contactos guardados aun...</p>
-                    <a href="./add.php">Agrega uno!!</a>
-                  </div>
-                </div>
-              <?php endif ?>
-                <?php foreach($contacts as $contact) : ?>
-                  <div class="col-md-4 mb-3">
-                    <div class="card text-center">
-                        <div class="card-body">
-                            <h3 class="card-title text-capitalize"><?= $contact["name"]?></h3>
-                            <p class="m-2"><?= $contact["phone_number"]?></p>
-                            <a href="./edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Edit Contact</a>
-                            <a href="./delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
-                        </div>
-                    </div>
-                </div>
-                <?php endforeach ?>
+  <div class="container pt-4 p-3">
+      <div class="row">
+        <?php if($contacts->rowCount() == 0): ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>Sin contactos guardados aun...</p>
+              <a href="./add.php">Agrega uno!!</a>
             </div>
-        </div>
-      </main>
+          </div>
+        <?php endif ?>
+          <?php foreach($contacts as $contact) : ?>
+            <div class="col-md-4 mb-3">
+              <div class="card text-center">
+                  <div class="card-body">
+                      <h3 class="card-title text-capitalize"><?= $contact["name"]?></h3>
+                      <p class="m-2"><?= $contact["phone_number"]?></p>
+                      <a href="./edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Edit Contact</a>
+                      <a href="./delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Delete Contact</a>
+                  </div>
+              </div>
+          </div>
+          <?php endforeach ?>
+      </div>
+  </div>
 
-
-
-
-
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-</body>
-</html>
+<?php require "partials/footer.php" ?>
